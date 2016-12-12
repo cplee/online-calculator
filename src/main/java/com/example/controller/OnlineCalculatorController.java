224f1d71a0a29abb95c08566b44832fe3dbbaf0d
Casey Lee <cplee@nektos.com>
2016-12-12 09:40:29 -0800

    baseline

--- src/main/java/com/example/controller/OnlineCalculatorController.java
+++ src/main/java/com/example/controller/OnlineCalculatorController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import javax.validation.ValidationException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,21 +12,32 @@ public class OnlineCalculatorController {
 
     @RequestMapping(value = "calculation", method = RequestMethod.POST)
     public ResponseEntity<Number> evaluate(@RequestBody String input) {
-    	// Parse string
+    	// Parse input string
         String regex = "(?<=op)|(?=op)".replace("op", "[-+*/]");
         String[] inputParts = input.replaceAll("\\s", "").split(regex);
         
-        // Assert valid number of parts
+        // Validate parts of input
         if(inputParts.length % 2 == 0) {
-        	return ResponseEntity.badRequest().body(0);
+        	return ResponseEntity.badRequest().body(Double.NaN);
+        }
+        
+        // Validate first value
+        double result;
+        try {
+        	result = Double.parseDouble(inputParts[0]);
+        } catch (NumberFormatException ex) {
+        	return ResponseEntity.badRequest().body(Double.NaN);
         }
     	
     	// Perform calculation
-    	double result = Double.parseDouble(inputParts[0]);
-    	
     	for(int i=1; i<inputParts.length; i++) {
     		String operator = inputParts[i];
-    		double operand = Double.parseDouble(inputParts[++i]);
+    		double operand;
+    		try {
+    			operand = Double.parseDouble(inputParts[++i]);
+	        } catch (NumberFormatException ex) {
+	        	return ResponseEntity.badRequest().body(Double.NaN);
+	        }
     		
     		if("+".equals(operator)) {
     			result += operand;
@@ -37,7 +47,7 @@ public class OnlineCalculatorController {
     			result *= operand;
     		} else if("/".equals(operator)) {
     			if(operand == 0) {
-    				return ResponseEntity.badRequest().body(0);
+    				return ResponseEntity.badRequest().body(Double.NaN);
     			}
     			result /= operand;
     		}
