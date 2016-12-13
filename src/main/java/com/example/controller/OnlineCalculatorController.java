5271f6dd1aea64ba6aef860f94848b628b851905
Casey Lee <cplee@nektos.com>
2016-12-12 23:17:06 -0800

    add tight coupling

--- src/main/java/com/example/controller/OnlineCalculatorController.java
+++ src/main/java/com/example/controller/OnlineCalculatorController.java
@@ -12,58 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OnlineCalculatorController {
+	private RoundingMode roundingMode = RoundingMode.HALF_UP;
+	private CalculatorService calculatorService = new CalculatorService();
 
 	@RequestMapping(value = "calculation", method = RequestMethod.POST)
 	public ResponseEntity<String> evaluate(
 			@RequestParam(defaultValue = "0") int scale, 
 			@RequestBody String input) {
-		// Parse input string
-		String regex = "(?<=op)|(?=op)".replace("op", "[-+*/^]");
-		String[] inputParts = input.split(regex);
-
-		// Validate parts of input
-		if (inputParts.length % 2 == 0) {
-			return ResponseEntity.badRequest().body("");
-		}
-
-		// Parse first value
-		double result;
-		try {
-			result = Double.parseDouble(inputParts[0]);
-		} catch (NumberFormatException ex) {
-			return ResponseEntity.badRequest().body("");
-		}
-
-		// Perform calculation
-		for (int i = 1; i < inputParts.length; i++) {
-			String operator = inputParts[i];
-			double operand;
-			try {
-				operand = Double.parseDouble(inputParts[++i]);
-			} catch (NumberFormatException ex) {
-				return ResponseEntity.badRequest().body("");
-			}
-
-			if ("+".equals(operator)) {
-				result += operand;
-			} else if ("-".equals(operator)) {
-				result -= operand;
-			} else if ("*".equals(operator)) {
-				result *= operand;
-			} else if ("/".equals(operator)) {
-				if (operand == 0) {
-					return ResponseEntity.badRequest().body("");
-				}
-				result /= operand;
-			} else if ("^".equals(operator)) {
-				result = Math.pow(result, operand);
-			} else {
-				return ResponseEntity.badRequest().body("");
-			}
-		}
-
-		// return result
-		RoundingMode roundingMode = RoundingMode.HALF_UP;
+		
+		double result = calculatorService.calculate(input);
 		return ResponseEntity.ok(BigDecimal.valueOf(result).setScale(scale, roundingMode).toString());
 	}
 }
--- src/test/java/com/example/controller/OnlineCalculatorControllerTest.java
+++ src/test/java/com/example/controller/OnlineCalculatorControllerTest.java
@@ -20,81 +20,11 @@ public class OnlineCalculatorControllerTest {
 	private MockMvc mvc;
 
 	@Test
-	public void testParseRequiresBody() throws Exception {
+	public void testComputeAddition() throws Exception {
 		this.mvc
-		.perform(post("/calculation"))
-		.andExpect(status().isBadRequest());
-	}
-	
-	@Test
-	public void testParseRequiresValidInput() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("1 + "))
-		.andExpect(status().isBadRequest());
-	}
-	
-	@Test
-	public void testComputeSimpleAddition() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("0 + 0"))
+		.perform(post("/calculation").content("2 + 2"))
 		.andExpect(status().isOk())
-		.andExpect(content().string("0"));
-	}
-	
-	@Test
-	public void testComputeSimpleSubtraction() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("3 - 2"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("1"));
-	}
-	
-	@Test
-	public void testComputeSimpleMultiplication() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("2 * 4"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("8"));
-	}
-	
-	@Test
-	public void testComputeSimpleDivision() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("9 / 3"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("3"));
-	}
-
-	@Test
-	public void testComputeDivideByZero() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("9 / 0"))
-		.andExpect(status().isBadRequest());
-	}
-	
-
-	@Test
-	public void testComputeComplexCalculation() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("3 + 5 - 2 * 4 / 8"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("3"));
-	}
-	
-	@Test
-	public void testRespondWithRoundingScale2() throws Exception {
-		this.mvc
-		.perform(post("/calculation").param("scale", "2").content("9.5 / 2"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("4.75"));
-	}
-	
-	@Test
-	public void testRespondWithRoundingDefaultScale() throws Exception {
-		this.mvc
-		.perform(post("/calculation").content("9.5 / 2"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("5"));
+		.andExpect(content().string("4"));
 	}
 	
 	@Test
